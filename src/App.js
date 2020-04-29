2f28e5d9162e469d27d81e5f28d4a2f3524038ce
abdel <a.akroh@e-durable.ch>
2020-04-29 12:06:24 +0200

    hackathon

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          Welcome on Hackathon 2020
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
